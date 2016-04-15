755a9547cb14b06a4ab65e10fac2f1b3833de703
ghostfart <g.b.maitland@gmail.com>
2016-04-15 13:12:13 +1200

    Add button action and a toast message for button press

--- app/src/main/java/nz/co/maitech/myappportfolio/MainActivity.java
+++ app/src/main/java/nz/co/maitech/myappportfolio/MainActivity.java
@@ -2,6 +2,9 @@ package nz.co.maitech.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void announce (View view) {
+        Button btn = (Button) view;
+        String msg = "This button will launch my " + btn.getText().toString().toLowerCase() + " app!";
+        Toast.makeText(this, msg,Toast.LENGTH_SHORT).show();
     }
 }
